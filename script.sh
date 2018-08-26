efa7d1944e4b73bbbe320a4fa6dcac3f2aa99f86
marisn <marisn@67cc.cn>
2018-08-26 20:24:13 +0800

    update
    update

--- script.sh
+++ script.sh
@@ -129,7 +129,7 @@ function aria_install(){
 	user-agent=Transmission/2.77
 	seed-time=0
 	bt-seed-unverified=true
-	on-download-complete=/root/.aria2/autoupload.sh
+	on-download-complete=/root/.aria2/OneIndexupload.sh
 	allow-overwrite=true
 	bt-tracker=udp://tracker.coppersurfer.tk:6969/announce,udp://tracker.open-internet.nl:6969/announce,udp://p4p.arenabg.com:1337/announce,udp://tracker.internetwarriors.net:1337/announce,udp://allesanddro.de:1337/announce,udp://9.rarbg.to:2710/announce,udp://tracker.skyts.net:6969/announce,udp://tracker.safe.moe:6969/announce,udp://tracker.piratepublic.com:1337/announce,udp://tracker.opentrackr.org:1337/announce,udp://tracker2.christianbro.pw:6969/announce,udp://tracker1.wasabii.com.tw:6969/announce,udp://tracker.zer0day.to:1337/announce,udp://public.popcorn-tracker.org:6969/announce,udp://tracker.xku.tv:6969/announce,udp://tracker.vanitycore.co:6969/announce,udp://inferno.demonoid.pw:3418/announce,udp://tracker.mg64.net:6969/announce,udp://open.facedatabg.net:6969/announce,udp://mgtracker.org:6969/announce" > /root/.aria2/aria2.conf
 }
